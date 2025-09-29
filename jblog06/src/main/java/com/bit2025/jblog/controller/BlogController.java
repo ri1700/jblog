9f2b6d7dab99b41a91303a7163efc5ffb82c637f
bit <bit@DESKTOP-HPBJEHN>
2025-09-29 17:34:51 +0900

    jblog

---
+++ jblog06/src/main/java/com/bit2025/jblog/controller/BlogController.java
@@ -0,0 +1,27 @@
+package com.bit2025.jblog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/{blogId:^(?!assets).*$}")
+public class BlogController {
+
+    @GetMapping({ "", "/", "/{categoryId}", "/{categoryId}/{postId}" })
+    public String main(
+            @PathVariable("blogId") String blogId,
+            @PathVariable(value = "categoryId", required = false) Long categoryId,
+            @PathVariable(value = "postId", required = false) Long postId) {
+
+        if (categoryId == null) {
+        	System.out.println("set default categoryId, default postId");
+        } else if (postId == null) {
+        	System.out.println("set default postId");
+        }
+    	
+    	return String.format("blogId:%s, categoryId: %d, postId: %d", blogId, categoryId, postId);
+    }
+
+}
